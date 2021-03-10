95dd465622d76ccb2ae66cdcb6c87e90d6fad238
yehuozhili <673632758@qq.com>
2021-03-10 15:42:45 +0800

    x

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,13 @@
+/*
+ * @Author: yehuozhili
+ * @Date: 2021-02-06 14:54:57
+ * @LastEditors: yehuozhili
+ * @LastEditTime: 2021-03-10 15:42:25
+ * @FilePath: \nextjsvercel\pages\api\hello.js
+ */
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+	res.statusCode = 200;
+	res.json({ name: "John Doe" });
+};
---
+++ pages/api/xxxx.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: yehuozhili
+ * @Date: 2021-03-10 15:42:21
+ * @LastEditors: yehuozhili
+ * @LastEditTime: 2021-03-10 15:42:22
+ * @FilePath: \nextjsvercel\pages\api\xxxx.js
+ */
+export default (req, res) => {
+	res.statusCode = 200;
+	res.json({ name: "John Dossse" });
+};
